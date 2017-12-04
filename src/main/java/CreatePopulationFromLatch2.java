e9fe6da20e22c94c6ee1506eefaf05b18809ac0f
kainagel <nagel@vsp.tu-berlin.de>
2017-12-04 14:48:28 +1100

    ...Latch2 more changes

--- src/main/java/CreatePopulationFromLatch2.java
+++ src/main/java/CreatePopulationFromLatch2.java
@@ -2,6 +2,14 @@
 import com.opencsv.bean.CsvBindByName;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationFactory;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,8 +26,14 @@ class CreatePopulationFromLatch2 {
 	
 	void run() throws IOException {
 		
+		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig() ) ;
+		final Population population = scenario.getPopulation();
+		PopulationFactory populationFactory = population.getFactory();;
+		
 		try (final FileReader reader = new FileReader(pusTripsFile)) {
 			// try-with-resources
+			
+			int cnt=0 ;
 			
 			final CsvToBeanBuilder<Visitors> builder = new CsvToBeanBuilder<>(reader);
 			builder.withType(Visitors.class);
@@ -27,9 +41,24 @@ class CreatePopulationFromLatch2 {
 			final CsvToBean<Visitors> reader2 = builder.build();
 			for (Iterator<Visitors> it = reader2.iterator(); it.hasNext(); ) {
 				Visitors record = it.next();
-				System.out.println( "AgentId=" + record.AgentId + "; rs=" + record.RelationshipStatus ) ;
+//				System.out.println( "AgentId=" + record.AgentId + "; rs=" + record.RelationshipStatus ) ;
+				
+				Person person = populationFactory.createPerson( Id.createPersonId(record.AgentId) );
+				population.addPerson(person);
+				
+				person.getAttributes().putAttribute( "RelationshipStatus" , record.RelationshipStatus ) ;
+				
+				if (cnt>=30) {
+					break ;
+				}
+				cnt++ ;
+				
 			}
+			
 		} // end of for loop
+		
+		PopulationWriter populationWriter = new PopulationWriter(scenario.getPopulation(), scenario.getNetwork());
+		populationWriter.write("population-from-latch.xml.gz");
 		
 	}
 	public final static class Visitors {
