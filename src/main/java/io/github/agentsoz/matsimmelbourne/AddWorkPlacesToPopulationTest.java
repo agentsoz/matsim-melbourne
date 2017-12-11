32668fae8fcb1a5d00bcaf97996050a5691df0ad
Karthikey Surineni <s3560603@student.rmit.edu.au>
2017-12-11 16:15:44 +1100

    refactored tests

--- src/main/java/io/github/agentsoz/matsimmelbourne/AddWorkPlacesToPopulationTest.java
+++ src/main/java/io/github/agentsoz/matsimmelbourne/AddWorkPlacesToPopulationTest.java
@@ -3,9 +3,6 @@ package io.github.agentsoz.matsimmelbourne;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.Scanner;
-
-import io.github.agentsoz.matsimmelbourne.AddWorkPlacesToPopulation;
 
 /**
  * Tests the AddWorkingPlacesToPopulation class
