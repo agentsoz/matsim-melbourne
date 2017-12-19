80422c259d344197c2f35355e5f0282d3bd62cd9
Karthikey Surineni <s3560603@student.rmit.edu.au>
2017-12-20 08:47:56 +1100

    Removed unused imports

--- src/test/java/io/github/agentsoz/matsimmelbourne/AddWorkPlacesToPopulationTest.java
+++ src/test/java/io/github/agentsoz/matsimmelbourne/AddWorkPlacesToPopulationTest.java
@@ -11,7 +11,10 @@ public class AddWorkPlacesToPopulationTest {
 
     @Test
     public final void testMain() throws IOException {
-        // FIXME: not really testing anything at the moment; also relies on availability of population-from-latch.xml which may not exist (think up a dir structure for this)
+        // FIXME: not really testing anything at the moment;
+        // ***********FIXED************ FIXME: also relies on availability of population-from-latch.xml which may not
+        // exist (think up a
+        // dir structure for this)
         // Maybe look at: https://github.com/agentsoz/bdi-abm-integration/blob/kaibranch/examples/bushfire/src/test/java/io/github/agentsoz/ees/FireAreaMaldon600Test.java
         AddWorkPlacesToPopulation.main(null);
 
--- src/test/java/io/github/agentsoz/matsimmelbourne/CreatePopulationFromLatchTest.java
+++ src/test/java/io/github/agentsoz/matsimmelbourne/CreatePopulationFromLatchTest.java
@@ -1,12 +1,6 @@
 package io.github.agentsoz.matsimmelbourne;
 
-import org.junit.Rule;
-import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.matsim.core.utils.misc.CRCChecksum;
-
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
