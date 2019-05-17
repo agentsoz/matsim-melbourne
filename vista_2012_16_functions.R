eeadedf59551ee38bbaeed2936036546491ef7b1
Dhirendra Singh <dhi.singh@gmail.com>
2019-05-17 14:44:56 +1000

    Code to create markov chain from activity sequences

--- vista_2012_16_functions.R
+++ vista_2012_16_functions.R
@@ -3,6 +3,7 @@ suppressMessages(library(ggplot2))
 suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
 suppressMessages(library(scales))
+suppressMessages(library(markovchain))
 
 extract_and_write_activities_from<-function(in_vista_csv, out_weekday_activities_csv_gz, out_weekend_activities_csv_gz) {
   gz1 <- gzfile(in_vista_csv,'rt')
@@ -96,8 +97,8 @@ simplify_activities_and_create_groups<-function(gzfile) {
   # 1. remove "Change Mode" activities which are just in-transit mode-change activities
   # 2. remove "Accompany Someone" which is a secondary activitiy
   df<-activities
-  df<-df[df$Activity!="Change Mode",]
-  df<-df[df$Activity!="Accompany Someone",]
+  #df<-df[df$Activity!="Change Mode",]
+  #df<-df[df$Activity!="Accompany Someone",]
   
   # Assign activities into groups as follows:
   df$Activity.Group<-""
@@ -110,6 +111,12 @@ simplify_activities_and_create_groups<-function(gzfile) {
   df$Activity.Group<-ifelse(
     df$Activity=="Work Related", 
     "Work", df$Activity.Group)
+  df$Activity.Group<-ifelse(
+    df$Activity=="Change Mode", 
+    "Mode Change", df$Activity.Group)
+  df$Activity.Group<-ifelse(
+    df$Activity=="Accompany Someone", 
+    "With Someone", df$Activity.Group)
   df$Activity.Group<-ifelse(
     df$Activity=="Education", 
     "Study", df$Activity.Group)
@@ -210,4 +217,58 @@ plot_week_activities_by_hour_of_day <- function(wd_activities_csv_gz, we_activit
     theme(legend.position = "bottom") +
     ggtitle(NULL)
   
+}
+
+create_markov_chain_model<-function(modelname, activities_csv_gz) {
+  gz1 <- gzfile(activities_csv_gz,'rt')
+  orig<-read.csv(gz1,header = T,sep=',',stringsAsFactors = F,strip.white = T)
+  close(gz1)
+  
+  activities<-orig
+  activities$Order<-order(activities$Person) # record order, used for filtering
+  # Rename 'Home' activities to 'Home Daytime'
+  activities[activities$Activity.Group=="Home",]$Activity.Group<-"Home Daytime"
+  # Rename start of the day 'Home' activities to 'Home Morning'
+  df<-activities
+  df<-aggregate(df,by=list(df$Person),FUN=head,1) # get first activities
+  df<-df[df$Activity.Group=="Home Daytime",] # remove all but home activities
+  activities[activities$Order%in%df$Order,]$Activity.Group<-"Home Morning" # rename those activities
+  # Rename end of the day 'Home' activities to 'Home Night'
+  df<-activities
+  df<-aggregate(df,by=list(df$Person),FUN=tail,1) # get last activities
+  df<-df[df$Activity.Group=="Home Daytime",] # remove all but home activities
+  activities[activities$Order%in%df$Order,]$Activity.Group<-"Home Night" # rename those activities
+  activities$Order<-NULL # done with temporary column
+  
+  # Get list of activities per person
+  activity.chains<-activities%>%
+    group_by(Person) %>% 
+    summarise(Activity.Chain = paste0(Activity.Group, collapse=","))
+  
+  states<-unique(sort(activities$Activity.Group))
+  probs<-data.frame(
+    matrix(0, nrow=length(states), ncol=length(states)),
+    row.names = states)
+  colnames(probs)<-states
+  for (j in 1:length(activity.chains$Activity.Chain)) {
+    chain<-activity.chains$Activity.Chain[j]
+    sq<-strsplit(chain, ",")[[1]]
+    if(length(sq)>1) {
+      for(i in 2:length(sq)) {
+        if (sq[i]%in%states && sq[i-1]%in%states) {
+          probs[sq[i-1],sq[i]]<-probs[sq[i-1],sq[i]]+1
+        } else {
+          print(paste0(sq[i-1],"|",sq[i]))
+        }
+      }
+    }
+  }
+  df<-probs
+  df["Home Night","Home Night"]<-1 # absorbing markov state
+  df<-df/rowSums(df)
+  mc<-new("markovchain", 
+          states = states,
+          transitionMatrix = as.matrix(df),
+          name="Activity Chains")
+  return(mc)
 }
