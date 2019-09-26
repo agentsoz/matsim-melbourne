3f13afe14fb1277d0e6e3320a8de78929b7535ee
jafshin <afshin.jafari@rmit.edu.au>
2019-09-26 11:16:33 +1000

    Solve issue with shapefile to igraph conversion

--- shp2graph/readshpnw.r
+++ shp2graph/readshpnw.r
@@ -1,8 +1,8 @@
 readshpnw<-function(ntdata, ELComputed=FALSE, longlat=FALSE, Detailed=FALSE, ea.prop=NULL)
-#The function returns a data list composed of "nodelist", "edgelist" and "data frame of edge attributes" 
+  #The function returns a data list composed of "nodelist", "edgelist" and "data frame of edge attributes" 
 {
   if(!is(ntdata,"SpatialLinesDataFrame"))
-     stop("Input data is not a proper spatial network data frame, here only SpatialLinesDataFrame is accepted.")
+    stop("Input data is not a proper spatial network data frame, here only SpatialLinesDataFrame is accepted.")
   Coords<-coordinates(ntdata)
   numEdges<-length(Coords)
   nodelist<-c()
@@ -16,24 +16,24 @@ readshpnw<-function(ntdata, ELComputed=FALSE, longlat=FALSE, Detailed=FALSE, ea.
   if (Detailed)
   {
     if (is.null(ea.prop))
-    stop("If a detailed graph is to be built, the properties of its attributes has to be specified")
+      stop("If a detailed graph is to be built, the properties of its attributes has to be specified")
     else
-      {
-         if (length(ea.prop)!=dim(Eadf)[2]-1)
-         stop("All the properties of attributs should be specified except EdgeID")
-      } 
+    {
+      if (length(ea.prop)!=dim(Eadf)[2]-1)
+        stop("All the properties of attributs should be specified except EdgeID")
+    } 
   }
-    #####################################################
+  #####################################################
   
   nodexlist<-vector(mode="double",length=0)
   nodeylist<-vector(mode="double",length=0)
   edgelength<-vector(mode="double",length=0)
   fromid<-0
   toid<-0
-##################################  
+  ##################################  
   if (Detailed)
   {
-     for (i in 1:numEdges)
+    for (i in 1:numEdges)
     {
       M<-dim(Coords[[i]][[1]])[1]
       SEl<-as.double(10)
@@ -51,29 +51,29 @@ readshpnw<-function(ntdata, ELComputed=FALSE, longlat=FALSE, Detailed=FALSE, ea.
         nodeylist<-c(nodeylist,ny)
       }
       for (j in 2:M)
-       {            
-          nx<-as.double(Coords[[i]][[1]][j,1])
-          ny<-as.double(Coords[[i]][[1]][j,2])
-          res1<-Update.nodelist(nodexlist,nodeylist, nodelist, nx, ny)
-          toid<-res1[[1]]
-          if(res1[[2]])
-           {
-            node<-list(res1[[1]],c(nx,ny))
-            nodelist<-rbind(nodelist,node)
-            nodexlist<-c(nodexlist,nx)
-            nodeylist<-c(nodeylist,ny)
-           }
-         ####Compute the edgelength
-           El<-as.double(0)
-           El<- .C("edgelength", as.double(Coords[[i]][[1]][c(j-1, j),1]),as.double(Coords[[i]][[1]][c(j-1, j),2]), as.integer(2), El, as.integer(longlat))[[4]]
-           ROL<-El/SEl
-           edgelist<-Update.edgelist(edgelist, edgeid, fromid, toid, ROL,Detailed=Detailed)
-           if(ELComputed)
-           {
-             edgelength<-c(edgelength,El)
-           }
-           fromid<-toid
-       }  
+      {            
+        nx<-as.double(Coords[[i]][[1]][j,1])
+        ny<-as.double(Coords[[i]][[1]][j,2])
+        res1<-Update.nodelist(nodexlist,nodeylist, nodelist, nx, ny)
+        toid<-res1[[1]]
+        if(res1[[2]])
+        {
+          node<-list(res1[[1]],c(nx,ny))
+          nodelist<-rbind(nodelist,node)
+          nodexlist<-c(nodexlist,nx)
+          nodeylist<-c(nodeylist,ny)
+        }
+        ####Compute the edgelength
+        El<-as.double(0)
+        El<- .C("edgelength", as.double(Coords[[i]][[1]][c(j-1, j),1]),as.double(Coords[[i]][[1]][c(j-1, j),2]), as.integer(2), El, as.integer(longlat))[[4]]
+        ROL<-El/SEl
+        edgelist<-Update.edgelist(edgelist, edgeid, fromid, toid, ROL,Detailed=Detailed)
+        if(ELComputed)
+        {
+          edgelength<-c(edgelength,El)
+        }
+        fromid<-toid
+      }  
     }    
   }
   else
@@ -105,20 +105,20 @@ readshpnw<-function(ntdata, ELComputed=FALSE, longlat=FALSE, Detailed=FALSE, ea.
       }
       edgeid<-edgeID.vec[i]
       edgelist<-Update.edgelist(edgelist, edgeid, fromid, toid, Detailed=Detailed)
-       ####Compute the edgelength
-           El<-as.double(0)
-           if(ELComputed)
-           {
-             El<- .C("edgelength", as.double(Coords[[i]][[1]][,1]),as.double(Coords[[i]][[1]][,2]), as.integer(M), El, as.integer(longlat))[[4]]
-             edgelength<-c(edgelength,El)
-           } 
+      ####Compute the edgelength
+      El<-as.double(0)
+      if(ELComputed)
+      {
+        El<- .C("edgelength", as.double(Coords[[i]][[1]][,1]),as.double(Coords[[i]][[1]][,2]), as.integer(M), El, as.integer(longlat))[[4]]
+        edgelength<-c(edgelength,El)
+      } 
     }    
   }
   if (Detailed)
   {
-     res<-extend.eadf(edgelist,Eadf, ea.prop)
-     edgelist<-res[[1]]
-     Eadf<-res[[2]]
+    res<-extend.eadf(edgelist,Eadf, ea.prop)
+    edgelist<-res[[1]]
+    Eadf<-res[[2]]
   }
   rownames(edgelist)<-NULL
   rownames(nodelist)<-NULL
@@ -127,45 +127,45 @@ readshpnw<-function(ntdata, ELComputed=FALSE, longlat=FALSE, Detailed=FALSE, ea.
 
 Update.nodelist<-function(nodexlist,nodeylist, nodelist, nx, ny)
 {
-   Nid<-length(nodexlist)
-   if(Nid==0)
-   {
-     Nid<-Nid+1
-     id<-as.integer(Nid)
-     isUpdate<-TRUE
-   }
-   else
-   {
-     tag<-as.integer(-1)
-     tag <- .C("nodeExisted", nodexlist,nodeylist, as.integer(Nid), nx, ny, tag)[[6]]
-     if (tag!=-1)
-        {
-          id<-tag
-          isUpdate<-FALSE
-        }
-        else
-        {
-          Nid<-Nid+1
-          id<-as.integer(Nid)
-          isUpdate<-TRUE
-        }
-   }
-   
-   res<-list(id,isUpdate)
-   res
+  Nid<-length(nodexlist)
+  if(Nid==0)
+  {
+    Nid<-Nid+1
+    id<-as.integer(Nid)
+    isUpdate<-TRUE
+  }
+  else
+  {
+    tag<-as.integer(-1)
+    tag <- .C("nodeExisted", nodexlist,nodeylist, as.integer(Nid), nx, ny, tag)[[6]]
+    if (tag!=-1)
+    {
+      id<-tag
+      isUpdate<-FALSE
+    }
+    else
+    {
+      Nid<-Nid+1
+      id<-as.integer(Nid)
+      isUpdate<-TRUE
+    }
+  }
+  
+  res<-list(id,isUpdate)
+  res
 }
 
 Update.edgelist<-function(Edgelist, edgeid, fromid, toid, ROL,Detailed)
 {
-   if (Detailed)
-   {
-     eid<-as.integer((length(Edgelist)%/%4)+1)
-     edge<-c(eid, edgeid, fromid, toid, ROL)  
-   }
-   else
-      edge<-c(edgeid, fromid, toid)
-   Edgelist<-rbind(Edgelist, edge)
-   Edgelist
+  if (Detailed)
+  {
+    eid<-as.integer((length(Edgelist)%/%4)+1)
+    edge<-c(eid, edgeid, fromid, toid, ROL)  
+  }
+  else
+    edge<-c(edgeid, fromid, toid)
+  Edgelist<-rbind(Edgelist, edge)
+  Edgelist
 }
 
 Nodes.coordinates<-function(nodelist=list())
@@ -175,8 +175,8 @@ Nodes.coordinates<-function(nodelist=list())
   Nodesy<-vector(mode="double",length=0)
   for (i in 1:Nn)
   {
-     Nodesx<-c(Nodesx, as.double(nodelist[i,2][[1]][1]))
-     Nodesy<-c(Nodesy, as.double(nodelist[i,2][[1]][2]))
+    Nodesx<-c(Nodesx, as.double(nodelist[i,2][[1]][1]))
+    Nodesy<-c(Nodesy, as.double(nodelist[i,2][[1]][2]))
   }
   Nodesxy<-cbind(Nodesx, Nodesy)
   colnames(Nodesxy)<-c("X","Y")
@@ -187,7 +187,7 @@ extend.eadf<-function(newedges,Eadf, ea.prop)
 {
   ##ver1.1
   if (is.null(dim(newedges)))
-      newedges<-matrix(newedges, nrow=1,byrow=T)
+    newedges<-matrix(newedges, nrow=1,byrow=T)
   ###
   id.idx<-1
   ne<-length(newedges[,1])
--- xmlWriter.R
+++ xmlWriter.R
@@ -1,12 +1,13 @@
-source("./shp2graph/nel2igraph.r")
-source("./shp2graph/readshpnw.r") # I needed to change the function a bit to also read the attributes as characters
+ # I needed to change the function a bit to also read the attributes as characters
 library(rgdal)
-
+library(igraph)
+library(shp2graph)
+source("./shp2graph/readshpnw.r")
 
 # plot(lines_filtered["modes"])
 
 # melb_filtered <- st_read("../../outputs/melbourne_filtered.shp") 
-melb_filtered <- readOGR("../../outputs/carlton/carlton.shp") # I am using rdgal instead of sf as shp2graph expects a spatialLinesDataFrame
+melb_filtered <- readOGR("../../../../OneDrive/OneDrive - RMIT University/Data/rawSpatial/shapeFiles/carlton/carlton.shp") # I am using rdgal instead of sf as shp2graph expects a spatialLinesDataFrame
 
 rtNEL1 <-readshpnw(melb_filtered, Detailed = TRUE, ea.prop = rep(1,9) ) # ea.prop is number of properties you want to keep
 igr1 <-nel2igraph(rtNEL1[[2]], rtNEL1[[3]]) # transforming to igraph
@@ -14,6 +15,6 @@ plot(igr1, vertex.label = NA, vertex.size = 0.5, vertex.size2 = 0.5, mark.col = 
      main = "The converted igraph graph")
 
 
-write_graph(igr1, file = "../../outputs/carlton/carlton1.txt", "graphml")
+write_graph(igr1, file = "../../../../OneDrive/OneDrive - RMIT University/Data/matsim-melbourne-AT/outputs/carlton2/carlton1.txt", "graphml")
 
 
