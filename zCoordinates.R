ef90d761c557f2c44d61975d735052d40e6a7d3f
jafshin <afshin.jafari@rmit.edu.au>
2019-10-22 12:28:46 +1100

    Adding Z coordinates reader function

--- zCoordinates.R
+++ zCoordinates.R
@@ -5,9 +5,16 @@ library(dplyr)
 
 crs_final <- 7845
 
+# Change to/not to save csv 
+saveCSV <- TRUE
+# Change to/not to save sqlite 
+saveSQLITE <- TRUE
+
+
+osm_extract <- "carltonSingleBlock"
+
 oneDriveURL <- "../../../../OneDrive/OneDrive - RMIT University"
-
-
+outputDir <- paste(oneDriveURL, "/Data/processedSpatial/", osm_extract,"/", sep= "")
 # Adding elevation --------------------------------------------------------
 
 elevation <- raster(paste(oneDriveURL, "/Data/rawSpatial/DEMs/DELWPx10.tif", sep = ""))
@@ -16,9 +23,28 @@ elevation <- raster(paste(oneDriveURL, "/Data/rawSpatial/DEMs/DELWPx10.tif", sep
 #elevation_small <- aggregate(elevation, fact = 2, fun = mean)
 
 
-my_nodes <- read.csv(file = paste(oneDriveURL, "/Data/processedSpatial/CBD_dockland/nodes_new.csv", sep = ""))
+my_nodes <- read.csv(file = paste(oneDriveURL, "/Data/processedSpatial/", osm_extract,"/", "nodes_new3.csv", sep = ""))
 
-my_points <- my_nodes %>%
-  mutate(points_h = st_point(c(my_points$x,my_points$y)))
 
-raster::extract(elevation_small ,as(my_point, "Spatial"),method='bilinear')
+
+my_nodes <- my_nodes %>% 
+              select(-X) %>%
+              st_as_sf(coords = c("x","y"))
+
+my_nodes$z <- round(raster::extract(elevation ,as(my_nodes, "Spatial"),method='bilinear'))/10
+
+
+if(saveSQLITE){
+  outputSQLite <- paste(outputDir ,osm_extract,"_points_elevated.sqlite", sep = "")
+  st_write(my_nodes, outputSQLite, layer = "lines", driver = "SQLite", layer_options = "GEOMETRY=AS_XY")
+  
+}
+
+if(saveCSV){
+  nodes_coords <- do.call(rbind, st_geometry(my_nodes))
+  colnames(nodes_coords) <- c("x", "y")
+  st_geometry(my_nodes) <- NULL 
+  my_nodes <- cbind(nodes_coords, my_nodes) %>% select(id, x, y, z) # if there is a column for nonplanrity it should be here
+  outputCSV <- paste(outputDir ,osm_extract,"_nodes_elevated.csv", sep = "")
+  write.csv(my_nodes, outputCSV)
+}
