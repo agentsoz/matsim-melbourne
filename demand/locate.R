139a3b2feb75d360239d89e11a5863ad3abe611d
alanboth <alan.both@rmit.edu.au>
2020-06-27 17:04:49 +1000

    Fixed crash in locate.R when it couldn't find a suitable region. Added function to calculate the SA1 distance between regions for locate.R. added locateVISTA.R, which validates the locate.R results provide you have access to the full VISTA survey. Finished integrating back probability into region selection (i.e., when selecting the next region, there should be a good chance of getting back home). This is about 4 times slower but could be optimised.

--- demand/locate.R
+++ demand/locate.R
@@ -48,6 +48,27 @@ assignActivityAreasAndTravelModes <-function(censuscsv, vistacsv, matchcsv, outd
     return(df)
   }
   
+  # nextModeAndSa1WithReturn(20607113903, 20701115539, "education", "car", FALSE)
+  nextModeAndSa1WithReturn <- function(homeSA1, fromSA1, toLocType, currentMode, allowModeChange) {
+    df<-NULL
+    returnProb <- 0
+    for (i in 1:10){
+      if (is.na(currentMode) || is.null(currentMode) || allowModeChange) { # mode can change if last activity was home
+        df<-findLocation(fromSA1, toLocType)
+      } else {
+        df<-findLocationKnownMode(fromSA1, toLocType, currentMode)
+      }
+      if (length(df) > 1) {
+        # df[2] = potential destination SA1, df[1] = potential travel mode
+        returnProb <- getReturnProbability(homeSA1,df[2],df[1])
+      }
+      # cat(paste0("\ndf1: ",df[1]," df2: ",df[2],", returnProb: ",returnProb,"\n"))
+      if (returnProb > 1) (return(df))
+    }
+    # cat(paste0("\nno suitable regions found, using one with a return probability of ",returnProb,"\n"))
+    return(df)
+  }
+  
   # Read in the persons
   gz1<-gzfile(censuscsv, 'rt')
   echo(paste0('Loading ABS census-like persons from ', censuscsv, '\n'))
@@ -67,7 +88,7 @@ assignActivityAreasAndTravelModes <-function(censuscsv, vistacsv, matchcsv, outd
   matches<-read.csv(gz1, header=T, stringsAsFactors=F, strip.white=T)
   close(gz1)
 
-  
+  # set.seed(20200406) # for when we want to have the same LocationType each time
   plans<- origplans %>%
     # Remove all plans that are not matched
     filter(PlanId %in% matches$PlanId) %>% 
@@ -78,7 +99,8 @@ assignActivityAreasAndTravelModes <-function(censuscsv, vistacsv, matchcsv, outd
     # Add location type tag
     mutate(LocationType=replaceActivityWithLocationTags(Activity)) %>%
     # Add a column for mode taken from last activity to this
-    mutate(ArrivingMode=NA)
+    mutate(ArrivingMode=NA) %>%
+    mutate(Distance=NA)
 
   echo('Assigning home SA1 locations\n')
   plans[plans$SA1_MAINCODE_2016!="",]$SA1_MAINCODE_2016<-
@@ -91,35 +113,52 @@ assignActivityAreasAndTravelModes <-function(censuscsv, vistacsv, matchcsv, outd
   discarded<-persons[FALSE,]
   doutfile<-paste0(outdir, '/persons.discarded.csv')
   write.table(discarded, file=doutfile, append=FALSE, row.names=FALSE, sep = ',')
-  wplans<-plans[1,]
+  wplans<-NULL
+  # pp<-plans[1:95,] # just the first 10 plans
   pp<-plans
-  i=1
+  i=0
+  homeSA1=NA
   processed<-0; ndiscarded<-0
+  # set.seed(20200406)
   while(i<nrow(pp)) {
     i<-i+1
-    startOfDay<-pp[i,]$PlanId != pp[i-1,]$PlanId && pp[i,]$LocationType=="home"
+    # cat(paste0(i,"\n"))
+    startOfDay<- i==1 || (pp[i,]$PlanId != pp[i-1,]$PlanId && 
+                          pp[i,]$LocationType=="home")
     endOfDay<- i==nrow(pp) || (pp[i,]$AgentId != pp[i+1,]$AgentId)
       
     if(startOfDay) {
+      homeSA1<-pp[i,]$SA1_MAINCODE_2016 # used for calculating return probabilities
       # nothing to do since home SA1s are already assigned; just save and continue
       wplans<-rbind(wplans, pp[i,])
     } else {
-      allowModeChange<- !endOfDay && pp[i,]$LocationType=="home" # allow mode change at home during the day
-      modeAndSa1<-nextModeAndSa1(as.numeric(pp[i-1,]$SA1_MAINCODE_2016), pp[i,]$LocationType, pp[i-1,]$ArrivingMode, allowModeChange)
+      # changed pp[i,] to pp[i-1,] since it's the 2nd entry with a mode (1st has no arriving mode)
+      allowModeChange<- !startOfDay && !endOfDay && pp[i-1,]$LocationType=="home" # allow mode change at home during the day
+      # SA1_MAINCODE_2016 and ArrivingMode should be using the previous leg's value
+      # modeAndSa1<-nextModeAndSa1(as.numeric(pp[i-1,]$SA1_MAINCODE_2016), pp[i,]$LocationType, pp[i-1,]$ArrivingMode, allowModeChange)
+      
+      # incorporating return propability into destination selection
+      modeAndSa1<-nextModeAndSa1WithReturn(homeSA1,as.numeric(pp[i-1,]$SA1_MAINCODE_2016), pp[i,]$LocationType, pp[i-1,]$ArrivingMode, allowModeChange)
       if(!is.null(modeAndSa1)) {
         # assign the mode and SA1
-        pp[i,]$ArrivingMode<-ifelse(is.null(modeAndSa1),NA,modeAndSa1[1])
-        pp[i,]$SA1_MAINCODE_2016<-ifelse(is.null(modeAndSa1),NA,modeAndSa1[2])
+        pp[i,]$ArrivingMode<-modeAndSa1[1]
+        # Need to keep the home SA1
+        if(pp[i,]$SA1_MAINCODE_2016 != homeSA1) {
+          pp[i,]$SA1_MAINCODE_2016<-modeAndSa1[2]
+        }
+        # add in the distance between the regions
+        pp[i,]$Distance <- calcDistance(pp[i-1,]$SA1_MAINCODE_2016,pp[i,]$SA1_MAINCODE_2016)
         # add it to our list
         wplans<-rbind(wplans, pp[i,])
       } else {
         # failed to find a suitable SA1/mode for this activity, so will just discard this person
         person<-persons[persons$AgentId==pp[i,]$AgentId,]
         discarded<-rbind(discarded,person)
-        # make all modes for this plan with 'x' (will delete these later)
+        # mark all modes for this plan with 'x' (will delete these later)
         pp[pp$PlanId==pp[i,]$PlanId,]$ArrivingMode<-'x'
-        # move to the first rown of the next plan
-        i<-as.numeric(rownames(last(pp[pp$PlanId==pp[i,]$PlanId,])))
+        # cat(paste0("\n","found an error at line ",i,"\n"))
+        # move to the first row of the next plan
+        i<-as.numeric(last(rownames(pp[pp$PlanId==pp[i,]$PlanId,])))
       }
     }
     # record progress for each person
@@ -159,4 +198,8 @@ runexample<- function() {
   writeInterval <- 100 # write to file every so many plans
   
   assignActivityAreasAndTravelModes(censuscsv, vistacsv, matchcsv, outdir, outcsv, writeInterval)
+  planToSpatial(read.csv("output/5.locate/plan.csv"),'output/5.locate/plan.sqlite')
+  
+  # only run if you have access to the full VISTA dataset
+  # source('locateVISTA.R', local=TRUE)
 }
--- demand/locations.R
+++ demand/locations.R
@@ -17,7 +17,7 @@ setkey(distanceMatrixIndex_dt, sa1_main16)
 # won't be used here. Joining with the distance matrix index so the regions are
 # in the correct order.
 SA1_attributed <- inner_join(distanceMatrixIndex,
-                             st_read("data/SA1_attributed.sqlite", quiet=TRUE),
+                             st_read("data/sa1_attributed.sqlite", quiet=TRUE),
                              by=c("sa1_main16"="sa1_mainco")) %>%
   dplyr::select(-GEOMETRY)
 SA1_attributed_dt<-data.table(SA1_attributed)
@@ -40,6 +40,7 @@ setkey(addresses_dt, sa1_main16)
 #                  distance and destination attractiveness. Distance is
 #                  currently weighted 4x higher since distance probability is a
 #                  lot more spread out.
+# calculateProbabilities(20604112202,"commercial","car")
 calculateProbabilities <- function(SA1_id,destination_category,mode) {
   # SA1_id=20604112202
   # destination_category="commercial"
@@ -80,19 +81,33 @@ calculateProbabilities <- function(SA1_id,destination_category,mode) {
   #distProbability <- distProbDensity/sum(distProbDensity, na.rm=TRUE) # normalising here so the sum of the probabilities equals 1
 
   # alternative way to compute distance probabilities for SA1s clipped to a much smaller set
-  # within 3 standard deviations of the mode mean - Dhi, 21/Feb/20
+  # within 2 standard deviations of the mode mean - Dhi, 21/Feb/20
   dd<-distances 
-  dd[dd<(modeMean-(2*modeSD)) | (dd>modeMean+(2*modeSD))]<- NA # discard anything >3SDs either side
-  dd[is.na(attractionProbDensity)] <- NA # discard regions with no valie destination types
+  dd[dd<(modeMean-(2*modeSD)) | (dd>modeMean+(2*modeSD))]<- NA # discard anything >2SDs either side
+  dd[is.na(attractionProbDensity)] <- NA # discard regions with no valid destination types
   if (sum(!is.na(dd)) == 0) return(NULL) # return NULL if nothing is left
   if(sum(!is.na(dd)) == 1) {
     dd[!is.na(dd)]<-1
   } else {
-    dd<-(max(dd, na.rm=TRUE)-dd)/max(dd, na.rm=TRUE) # prob of visiting based on distance
+    #  changed this to a z-score based method - Alan, 22/Jun/20
+    dd<-(2-abs((dd-modeMean)/modeSD))
+    # dd<-(max(dd, na.rm=TRUE)-dd)/max(dd, na.rm=TRUE) # prob of visiting based on distance
     dd<-dd/sum(dd, na.rm=TRUE)
   }
   distProbability<-dd
 
+  # distComparison <- data.frame(sa1=SA1_attributed$sa1_main16,
+  #                              distances,
+  #                              dd,
+  #                              dhi = (max(dd, na.rm = TRUE) - dd) / max(dd, na.rm = TRUE),
+  #                              z_score = 2 - abs((dd - modeMean) / modeSD),
+  #                              norm = dnorm(dd, mean = modeMean, sd = modeSD)
+  # )
+  # distComparison <- distComparison %>%
+  #   mutate(dhi = dhi / sum(distComparison$dhi,na.rm=TRUE),
+  #          z_score = z_score / sum(distComparison$z_score,na.rm=TRUE),
+  #          norm = norm / sum(distComparison$norm,na.rm=TRUE))
+  
   # I've set distance probability to 4x more important than destination 
   # attraction. This is arbitrary.
   multiplier=1 #  changed this from 4 to 1 - Dhi, 21/Feb/20
@@ -108,12 +123,12 @@ calculateProbabilities <- function(SA1_id,destination_category,mode) {
 
 # This will be made more detailed later, and actually take destination category
 # into account.
+# chooseMode(20604112202,"commercial")
 chooseMode <- function(SA1_id,destination_category) {
   # SA1_id=20604112202
   # destination_category="commercial"
 
-  #modeProbability <- SA1_attributed %>%
-  #  filter(sa1_main16==SA1_id) %>%
+  # a list of the four mode probabilities for this SA1
   modeProbability <- SA1_attributed_dt[.(as.numeric(SA1_id))] %>%
     dplyr::select(bicycle_proportion:walking_proportion) %>%
     unlist()
@@ -127,6 +142,7 @@ chooseMode <- function(SA1_id,destination_category) {
 }
 
 # Assuming the transport mode is restricted, this will find a destination SA1
+# findLocationKnownMode(20604112202,"commercial","car")
 findLocationKnownMode <- function(SA1_id,destination_category,mode) {
   #cat(paste0("\nSA1_id=[",SA1_id,"] destination_category=[",destination_category,"] mode=[",mode,"]\n"))
   probabilityDF <- calculateProbabilities(SA1_id,destination_category,mode)
@@ -142,22 +158,26 @@ findLocationKnownMode <- function(SA1_id,destination_category,mode) {
 }
 
 # Find a destination SA1 given a source SA1 and destination category
+# findLocation(20604112202,"commercial")
 findLocation <- function(SA1_id,destination_category) {
   return(findLocationKnownMode(SA1_id,destination_category,chooseMode(SA1_id,destination_category)))
 }
 
 
 # Determine the chances of returning home for a given destination and transport mode
-getReturnProbability <- function(source_SA1,destination_SA1,destination_category,mode) {
-  # source_SA1=20604112202
-  # destination_SA1=20604112210
-  # destination_category="commercial"
-  mode="car"
-  probabilityDF <- calculateProbabilities(destination_SA1,destination_category,mode)
+# getReturnProbability(20604112202,20604112210,"commercial")
+getReturnProbability <- function(source_SA1,destination_SA1,mode) {
+  # source_SA1=20607113903
+  # destination_SA1=20803119308
+  # mode="car"
+  probabilityDF <- calculateProbabilities(destination_SA1,"home",mode)
   sourceProb <- probabilityDF %>%
     filter(sa1_main16==source_SA1) %>%
     pull(distProb) # Note that we only use the distance probability here, not
                    # the combined probability.
+  # sourceProb only returns regions within 2sd of the mean, so if the 
+  # destination is too far away, we need to manually set the probability to zero
+  if(length(sourceProb)==0) (sourceProb=0)
   sourceProb <- sourceProb*nrow(probabilityDF)
   # multiplying by the number of possible regions to give the proportion of
   # choosing that region, where > 1 is more likely than choosing the region
@@ -186,8 +206,41 @@ getAddressCoordinates <- function(SA1_id,destination_category) {
   return(address_coordinates)
 }
 
+# Returns the distance between two regions
+calcDistance <- function(from_sa1,to_sa1) {
+  index1 <- distanceMatrixIndex_dt[.(as.numeric(from_sa1))] %>%
+    pull(index)
+  index2 <- distanceMatrixIndex_dt[.(as.numeric(to_sa1))] %>%
+    pull(index)
+  return(distanceMatrix[index1,index2])
+}
 
-
+# Takes a plan with completed SA1 locations and turns them into a series of
+# lines where the non-spatial data is for the destination.
+# Need to supply a plan and an output file location. I recommend using the 
+# .sqlite extension instead of shapefiles.
+# planToSpatial(read.csv("output/5.locate/plan.csv"),'output/5.locate/plan.sqlite')
+planToSpatial <- function(pp,fileLocation) {
+  
+  # Need the x and y locations of the centroids
+  sa1_centroids <- st_read("data/sa1_centroids.sqlite",
+                           query="SELECT sa1_main16,x,y FROM sa1_centroids")
+  ppp <- pp %>%
+    mutate(SA1_MAINCODE_2016=as.numeric(SA1_MAINCODE_2016)) %>%
+    # Need the previous SA1 region
+    mutate(prev_sa1=lag(SA1_MAINCODE_2016)) %>%
+    # Ignore the first entries for a person as they won't have a valid prev SA1
+    filter(!is.na(ArrivingMode)) %>%
+    inner_join(sa1_centroids, by=c("prev_sa1"="sa1_main16")) %>%
+    inner_join(sa1_centroids, by=c("SA1_MAINCODE_2016"="sa1_main16")) %>%
+    # turn the two SA1 centroids into line geometry
+    mutate(GEOMETRY=paste0("LINESTRING(",x.x," ",y.x,",",x.y," ",y.y,")")) %>%
+    st_as_sf(wkt = "GEOMETRY", crs = 28355) %>%
+    dplyr::select(PlanId,Activity,StartBin,EndBin,AgentId,SA1_MAINCODE_2016,
+                  LocationType,ArrivingMode,Distance)
+  # Write the spatial dataframe to file
+  st_write(ppp,fileLocation,delete_dsn=TRUE)
+}
 
 # EXAMPLES
 
