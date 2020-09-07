6621eb9955a8a1009c0e8103399894087422ceed
Dhirendra Singh <dhi.singh@gmail.com>
2020-09-08 09:33:47 +1000

    Moved network generation code

--- network/functions/addElevation2Nodes.R
+++
@@ -1,6 +0,0 @@
-addElevation2Nodes <- function(nodes, rasterFile){
-  elevation <- raster(rasterFile) 
-  nodes$z <- round(raster::extract(elevation ,as(nodes, "Spatial"),method='bilinear'))/10
-  
-  return(nodes)
-}
